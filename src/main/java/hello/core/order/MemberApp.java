8140114eb387dfdc4eca1ba89a780f1eed11a734
kdjidkr <kcwidrk@naver.com>
2026-03-08 15:11:39 +0900

    Feat : 섹션 4
    - 섹션 4 까지의 내용을 작성함

--- src/main/java/hello/core/order/MemberApp.java
+++ src/main/java/hello/core/order/MemberApp.java
@@ -4,23 +4,24 @@ import hello.core.AppConfig;
 import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
-import hello.core.member.MemberServiceImpl;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
-public class OrderApp {
+public class MemberApp {
     public static void main(String[] args) {
 //        AppConfig appConfig = new AppConfig();
 //        MemberService memberService = appConfig.memberService();
-        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
-        OrderService orderService = applicationContext.getBean("orderService", OrderService.class);
-        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
-        Long memberId = 1L;
-        Member member = new Member(memberId, "memberA", Grade.VIP);
-        memberService.join(member);
-        Order order = orderService.createOrder(memberId, "itemA", 10000);
 
-        System.out.println("order = " + order);
-        System.out.println("order = " + order.calculatePrice());
+        // AppConfig에 있는 모든 설정 정보들을 Spring 컨테이너에 넣어서 관리해줌
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        // getBean (메서드 이름, 타입)
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+
+        Member member = new Member(1L, "memberA", Grade.VIP);
+        memberService.join(member);
+
+        Member findMember = memberService.findMember(1L);
+        System.out.println("new Member = " + member.getName());
+        System.out.println("find member = " + findMember.getName());
     }
 }
