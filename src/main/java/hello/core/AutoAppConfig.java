de49f99bffe197cd2be681c67e2b58e29b38f35d
kdjidkr <kcwidrk@naver.com>
2026-03-13 13:04:27 +0900

    Feat : 섹션 7
    - 섹션 7 까지의 내용을 작성함

---
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -0,0 +1,22 @@
+package hello.core;
+
+import hello.core.member.MemberRepository;
+import hello.core.member.MemoryMemberRepository;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+
+
+@Configuration
+@ComponentScan(
+        basePackages = "hello.core.member",
+        // AppConfig에서 Configuration으로 등록된 설정정보가 자동 컴포넌트스캔이 돼어서 빈이 등록되는 것을 방지하고자 exclude 넣어주기
+        excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
+)
+public class AutoAppConfig {
+//    @Bean(name = "memoryMemberRepository")
+//    MemberRepository memberRepository() {
+//        return new MemoryMemberRepository();
+//    }
+}
